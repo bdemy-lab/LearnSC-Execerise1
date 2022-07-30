fe731483ca3f1a979541023efcbc70340aa231c2
Nguyễn Minh Tuấn <68626789+tuannm1006@users.noreply.github.com>
2022-07-30 20:30:37 +0700

    Add files via upload

--- MultiSender.sol
+++ MultiSender.sol
@@ -1,19 +1,48 @@
 // SPDX-License-Identifier: None
 pragma solidity ^0.8.0;
 
-import "./SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "hardhat/console.sol";
 
 contract MultiSender {
-    using SafeMath for uint256;
+    bool private locked;
 
     event Transfer(
         address indexed from,
         address indexed to,
         uint256 indexed amount
     );
+
+    event TransferError(
+        address indexed from,
+        address indexed to,
+        uint256 indexed amount
+    );
+
+    event TransferERC721Error(
+        address indexed from,
+        address indexed to,
+        uint256 indexed id
+    );
+
+    modifier nonReentrant() {
+        require(!locked, "Reentrant call");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    modifier matchReceiver(
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) {
+        require(recipients.length > 0, "Recipients is empty");
+        require(
+            recipients.length == amounts.length,
+            "Recipients not match amounts"
+        );
+        _;
+    }
 
     /**
      * @dev Transfer ethers to multi sender
@@ -23,33 +52,20 @@ contract MultiSender {
      * @param amounts List of amount
      */
     function multiSendEth(
-        address payable[] memory recipients,
-        uint256[] memory amounts
-    ) public payable {
-        require(recipients.length > 0, "Recipients is empty");
-        require(
-            recipients.length == amounts.length,
-            "Recipients not match token ids"
-        );
-
-        uint256 totalAmount = 0;
-
-        // calculate total amount
-        for (uint256 i = 0; i < amounts.length; i++) {
-            require(amounts[i] > 0);
-            totalAmount = totalAmount.add(amounts[i]);
-        }
-
-        // check msg.value at least total amount
-        require(totalAmount <= msg.value, "Not enough amount");
-
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) public payable nonReentrant matchReceiver(recipients, amounts) {
         // send
         for (uint256 i = 0; i < recipients.length; i++) {
             if (recipients[i] != address(0)) {
-                bool success = recipients[i].send(amounts[i]);
+                (bool success, ) = payable(recipients[i]).call{
+                    value: amounts[i]
+                }("");
 
                 if (success) {
                     emit Transfer(msg.sender, recipients[i], amounts[i]);
+                } else {
+                    emit TransferError(msg.sender, recipients[i], amounts[i]);
                 }
             }
         }
@@ -65,54 +81,18 @@ contract MultiSender {
      */
     function multiSendERC20(
         address token,
-        address[] memory recipients,
-        uint256[] memory amounts
-    ) public {
-        require(recipients.length > 0, "Recipients is empty");
-        require(
-            recipients.length == amounts.length,
-            "Recipients not match amounts"
-        );
-
+        address[] calldata recipients,
+        uint256[] calldata amounts
+    ) public matchReceiver(recipients, amounts) {
         address from = msg.sender;
         IERC20 erc20 = IERC20(token);
-        uint256 totalAmount = 0;
-        uint256 allowance = 0;
-        uint256 balance = 0;
-
-        // get allownce
-        try erc20.allowance(from, address(this)) returns (uint256 amount) {
-            allowance = amount;
-            console.log("Allowance: ", allowance);
-        } catch Error(string memory reason) {
-            console.log("Error: ", reason);
-        }
-
-        // get sender balance
-        try erc20.balanceOf(from) returns (uint256 amount) {
-            balance = amount;
-        } catch Error(string memory reason) {
-            console.log("Error: ", reason);
-        }
-
-        // calculate total amount
-        for (uint256 i = 0; i < amounts.length; i++) {
-            require(amounts[i] > 0);
-            totalAmount = totalAmount.add(amounts[i]);
-        }
-
-        // check allowance and balance at least total amount
-        require(totalAmount <= allowance, "Not enough allowance");
-        require(totalAmount <= balance, "Not enough balance");
 
         // send
         for (uint256 i = 0; i < recipients.length; i++) {
-            if (recipients[i] != address(0)) {
-                try
-                    erc20.transferFrom(from, recipients[i], amounts[i])
-                {} catch Error(string memory reason) {
-                    console.log("Error: ", reason);
-                }
+            try
+                erc20.transferFrom(from, recipients[i], amounts[i])
+            {} catch Error(string memory reason) {
+                emit TransferError(msg.sender, recipients[i], amounts[i]);
             }
         }
     }
@@ -127,8 +107,8 @@ contract MultiSender {
      */
     function multiSendERC721(
         address token,
-        address[] memory recipients,
-        uint256[] memory ids
+        address[] calldata recipients,
+        uint256[] calldata ids
     ) public {
         require(recipients.length > 0, "Recipients is empty");
         require(
@@ -140,12 +120,10 @@ contract MultiSender {
         IERC721 erc721 = IERC721(token);
 
         for (uint256 i = 0; i < recipients.length; i++) {
-            if (recipients[i] != address(0)) {
-                try
-                    erc721.safeTransferFrom(from, recipients[i], ids[i])
-                {} catch Error(string memory reason) {
-                    console.log("Error: ", reason);
-                }
+            try
+                erc721.safeTransferFrom(from, recipients[i], ids[i])
+            {} catch Error(string memory reason) {
+                emit TransferERC721Error(msg.sender, recipients[i], ids[i]);
             }
         }
     }
